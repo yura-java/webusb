d973a21a80073fc914dc25197fcf5eaa0b25efba
Yuriy Nakonechnyy <yuriy@sombrainc.com>
2019-06-08 17:38:05 +0300

    Added list of devices

--- myscript.js
+++ myscript.js
@@ -1,11 +1,33 @@
+function rerenderList(devices) {
+	let list = document.getElementById('devices_list')
+	removeChildren(list)
+	for (device in devices) {
+		let deviceNode = document.createElement('li')
+		deviceNode.innerHTML = "<b>Device:</b> " + device.manufacturerName + " " + device.productName;
+		list.appendChild(deviceNode)
+	}
+}
+
+function removeChildren(node) {
+	while (node.firstChild) {
+	    node.removeChild(node.firstChild);
+	}
+	
+}
+
 document.addEventListener('DOMContentLoaded', event => {
 	let button = document.getElementById('connect')
 
+	let devices = {}
+	
 	button.addEventListener('click', async() => {
 		console.log("click")
 		
-		navigator.usb.requestDevice({filters:[]}).then(function(device){
+		navigator.usb.requestDevice({filters:[]}).then(device => {
 			console.log(device);
+			
+			let deviceId = [ device.manufacturerName, device.productId, device.productName, device.serialNumber, device.vendorId ];
+			devices[deviceId] = device;
 		});
 	})
 })
